0a50108a19d49acb27c91e2761e7e46e1362d490
Rupe-Rosicky <rupe@rupertward.co.uk>
2015-07-26 18:14:53 +0100

    Initial Pulse stuff

---
+++ Assets/Scripts/World/Pulse.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(Rigidbody))]
+public class Pulse : MonoBehaviour 
+{
+
+#region public settings
+
+	public bool useGravity = false;
+
+	public Vector3 velocity = Vector3.zero;
+	
+#endregion public settings
+
+#region private Hooks
+
+	private Rigidbody rigidbody_;
+
+#endregion private Hooks
+
+
+#region SetUp
+
+	public void Init(Vector3 pVelocity, bool pUseGravity)
+	{
+		gameObject.SetActive(true);
+
+		velocity = pVelocity;
+		useGravity = false;
+		rigidbody_.useGravity = useGravity;
+		rigidbody_.velocity = velocity;
+	}
+	
+	void Awake()
+	{
+		rigidbody_ = GetComponent< Rigidbody >();
+	}
+
+	void Start () 
+	{
+	}
+
+#endregion SetUp
+
+#region Flow
+
+	void FixedUpdate () 
+	{
+		transform.localPosition = transform.localPosition + velocity * Time.fixedDeltaTime;
+	}
+
+#endregion Flow
+
+}
---
+++ Assets/Scripts/World/PulseGenerator.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PulseGenerator : MonoBehaviour 
+{
+#region Inspector hooks
+
+	public GameObject pulseTemplate;
+	public GameObject pulseHolder;
+	public Transform pulseStartPosition;
+
+	public float frequencyHz = 2f;
+
+	public bool useGravity = false;
+	public Vector3 pulseVelocity = Vector3.left;
+	public float pulseSpeed = 1f;
+
+
+#endregion Inspector hooks
+
+#region private data
+
+	private float timeTillPulse_ = 0f;
+
+	private uint nextPulseNumber_ = 0u;
+
+#endregion private data
+
+#region SetUp
+
+	public void Awake()
+	{
+		pulseVelocity = pulseVelocity / pulseVelocity.magnitude;
+		pulseVelocity *= pulseSpeed;
+	}
+
+#endregion SetUp
+
+#region Flow
+
+	public void Update()
+	{
+		timeTillPulse_ -= Time.deltaTime;
+		if (timeTillPulse_ <= 0f)
+		{
+			CreatePulse();
+			timeTillPulse_ = 1f / frequencyHz;
+		}
+	}
+
+#endregion Flow
+
+#region Pulse
+	
+	private void CreatePulse()
+	{
+		GameObject newPulseGO = Instantiate(pulseTemplate) as GameObject;
+		newPulseGO.name = "Pulse_" + nextPulseNumber_.ToString();
+		newPulseGO.transform.parent = pulseHolder.transform;
+		newPulseGO.transform.position = pulseStartPosition.position;
+		newPulseGO.transform.localScale = Vector3.one;
+
+		nextPulseNumber_++;
+
+		Pulse newPulse = newPulseGO.GetComponent< Pulse >();
+		newPulse.Init(pulseVelocity, useGravity);
+
+	}
+			
+#endregion Pulse
+			
+
+}
