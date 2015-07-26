9a7a4b3ba5e57e5149133218f560a6845874c2c6
Rupe-Rosicky <rupe@rupertward.co.uk>
2015-07-26 18:44:27 +0100

    Basic pulse detector

---
+++ Assets/Scripts/World/PulseDetector.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PulseDetector : MonoBehaviour 
+{
+	static private readonly bool DEBUG_LOCAL = true;
+
+#region inspector hooks
+
+
+#endregion inspector hooks
+
+#region public settings
+
+	public Color normalColour = Color.white;
+	public Color pulseColour = Color.red;
+
+	public int killsPulseFrequency = 0;
+
+#endregion public settings
+
+#region private hooks
+
+
+#endregion private hooks
+
+#region private data
+
+	private int pulsesSinceKill = 0;
+
+#endregion private data
+
+#region Unity event handler
+
+	public void OnTriggerEnter(Collider other)
+	{
+		if (DEBUG_LOCAL)
+		{
+			Debug.Log(other.gameObject.name+" triggered "+this.gameObject.name);
+		}
+		if (killsPulseFrequency > 0)
+		{
+			pulsesSinceKill++;
+			if (pulsesSinceKill >= killsPulseFrequency)
+			{
+				GameObject.Destroy(other.gameObject);
+				pulsesSinceKill = 0;
+			}
+		}
+	}
+#endregion Unity event handler
+
+}
